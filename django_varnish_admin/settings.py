b0dc4046ff2f93f18799133242e92f9383bb0cdb
Vicente Málaga <vmalaga@qdqmedia.com>
2013-02-19 11:01:04 +0100

    Change to sqlite3 from mysql

--- django_varnish_admin/settings.py
+++ django_varnish_admin/settings.py
@@ -13,11 +13,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'djvaradm',                      # Or path to database file if using sqlite3.
-        'USER': 'djvaradm',                      # Not used with sqlite3.
-        'PASSWORD': 'djvaradm',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'djvaradm.dbf',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
