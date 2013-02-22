1a831df720eea49b5490359602dbbd0c3879fd09
Vicente Málaga <vmalaga@qdqmedia.com>
2013-02-22 12:09:42 +0100

    error on static url

--- django_varnish_admin/urls.py
+++ django_varnish_admin/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 
 # Uncomment the next two lines to enable the admin:
@@ -22,7 +23,7 @@ urlpatterns = patterns('',
     (r'^logout/$', 'frontend.views.logout_page'),
 
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-)
+    
 
 
 )
